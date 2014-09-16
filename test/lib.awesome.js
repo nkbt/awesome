eba99fe1ad06588c6adeb8c8cc1078153892f723
Nik Butenko <nik@butenko.me>
2014-09-16 23:48:55 +1000

    Rename NPM-Template to Awesome

--- test/lib.awesome.js
+++ test/lib.awesome.js
@@ -7,53 +7,53 @@ var sinon = require('sinon');
 
 var path = require('path');
 var expect = require('chai').expect;
-var NpmTemplate = require(path.join(__dirname, '..', 'index'));
+var Awesome = require(path.join(__dirname, '..', 'index'));
 
-describe('NPM Template', function () {
-
-
-	it('should respond to [getOptions, setOptions] instance methods', function () {
-		expect(NpmTemplate).to.respondTo('getOptions', 'setOptions');
-	});
+describe('Awesome', function () {
 
 
-	it('should have [getDefaults] static method', function () {
-		expect(NpmTemplate).itself.to.respondTo('getDefaults');
-	});
+  it('should respond to [getOptions, setOptions] instance methods', function () {
+    expect(Awesome).to.respondTo('getOptions', 'setOptions');
+  });
 
 
-	it('should have [errors] static property', function () {
-		expect(NpmTemplate).to.have.property('errors');
-	});
+  it('should have [getDefaults] static method', function () {
+    expect(Awesome).itself.to.respondTo('getDefaults');
+  });
 
 
-	it('should have default options pre-set', function () {
-		var npmTemplate = new NpmTemplate();
-		expect(npmTemplate.getOptions()).to.deep.equal(NpmTemplate.getDefaults());
-	});
+  it('should have [errors] static property', function () {
+    expect(Awesome).to.have.property('errors');
+  });
 
 
-	it('should have options.name set to "Test"', function () {
-		var npmTemplate = new NpmTemplate({name: 'Test'});
-		expect(npmTemplate.getOptions().name).to.equal('Test');
-	});
+  it('should have default options pre-set', function () {
+    var awesome = new Awesome();
+    expect(awesome.getOptions()).to.deep.equal(Awesome.getDefaults());
+  });
 
 
-	it('should not set any unexpected options', function () {
-		var npmTemplate = new NpmTemplate({hello: 'World'});
-		expect(npmTemplate.getOptions()).not.to.have.keys('hello');
-	});
+  it('should have options.name set to "Test"', function () {
+    var awesome = new Awesome({name: 'Test'});
+    expect(awesome.getOptions().name).to.equal('Test');
+  });
 
 
-	it('should emit OptionsRequiredError if no options passed', function () {
-		var npmTemplate = new NpmTemplate(),
-			errorSpy = sinon.spy();
+  it('should not set any unexpected options', function () {
+    var awesome = new Awesome({hello: 'World'});
+    expect(awesome.getOptions()).not.to.have.keys('hello');
+  });
 
-		npmTemplate.on('error', errorSpy);
 
-		npmTemplate.setOptions();
-		expect(errorSpy).to.be.calledOnce;
-	});
+  it('should emit OptionsRequiredError if no options passed', function () {
+    var awesome = new Awesome(),
+      errorSpy = sinon.spy();
+
+    awesome.on('error', errorSpy);
+
+    awesome.setOptions();
+    expect(errorSpy).to.be.calledOnce;
+  });
 
 
 });
